4f164eee59a656c6c1156996192395c059964213
Dreamboy-26 <heyimpankaj2616@gmail.com>
2024-12-27 20:08:25 +0530

    user authentication done

--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,23 @@ const express=require('express')
 const app= express();
 const dotenv=require('dotenv')
 dotenv.config();
-
+const connectToDb=require("./db/db")
 const cors=require('cors')
-
+const userRoutes=require("./routes/user.routes")
+const cookieParser=require("cookie-parser")
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+connectToDb()
 app.use(cors());
+app.use(cookieParser());
 
 
+
+app.get("/", (req,res)=>{
+    res.send("Hello Panakj")
+})
+
+app.use("/users",userRoutes)
 
 
 module.exports=app;
